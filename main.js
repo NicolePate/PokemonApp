04ce1f4dc16664027ffcb273f1c8a80935151c29
NicolePate <nicole.pate512@gmail.com>
2020-06-21 18:42:14 -0500

    updated js, basic css

--- main.js
+++ main.js
@@ -1,16 +1,87 @@
-//require('dotenv').config()
+// require('dotenv').config()
 
 window.onload = function() {
-    getPokemon();
-}
-let arrayOfPokemon = [];
+    
+    
+    }
 
-const getPokemon = () => {
-    let irandom = Math.floor(Math.random() * 150)
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=5&offset='+irandom)
+pokemon = [];
+
+
+// creates a pull from API of 200 randomize pokemon. Once onclick is selected it displays 
+// pokemon type, and image
+const getPokemonOne = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=200&offset=200')
     .then(res => res.json())
-    .then(user => user.results.map(pokemon => {
-        arrayOfPokemon.push(pokemon)
-        console.log(pokemon)
-    }));
+    .then(data => {
+        let random = Math.floor(Math.random()*200);
+        return fetch(data.results[random].url)
+    })
+    .then(res => res.json())
+    .then(data => {
+        pokemon.push(data)
+        console.log(pokemon);
+    })
+    .then(data => {
+        pokemonData = data;
+        const one = document.getElementById("one");
+        const li = document.createElement("li");
+        const pic = document.createElement("img");
+        pic.setAttribute("src", `${pokemon[0].sprites.front_default}`);
+        li.appendChild(document.createTextNode(pokemon[0].species.name));
+        li.appendChild(pic);
+        one.append(li)
+    })
 }
+// will pull second Pokemon from  API of 200 randomize pokemon. Once onclick is selected it displays 
+// pokemon type, and image
+const getPokemonTwo = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=200&offset=200')
+    .then(res => res.json())
+    .then(data => {
+        let random = Math.floor(Math.random()*200);
+        return fetch(data.results[random].url)
+    })
+    .then(res => res.json())
+    .then(data => {
+        pokemon.push(data)
+        console.log(pokemon);
+    })
+    .then(data => {
+        pokemonData = data;
+        const two = document.getElementById("two");
+        const pic = document.createElement("img");
+        const li = document.createElement("li");
+        pic.setAttribute("src", `${pokemon[1].sprites.front_default}`);
+        li.appendChild(document.createTextNode(pokemon[1].species.name));
+        li.appendChild(pic);
+        two.append(li)
+    })
+}
+
+//a function to conpare the 2 pokemon picked at random and compares there stats. The 
+// pokemon with the higher stats wins. 
+// ?can not get the image of the winning pokemon to appear?
+const battlePokemon= () => {
+    
+    if ((pokemon[0].stats[1].base_stat) > (pokemon[1].stats[1].base_stat)) {
+        winner = pokemon[0].species.name
+    }
+    else {
+        winner = pokemon[1].species.name
+       
+    }
+    const pokemonWinner = document.getElementById('winner');
+        const li = document.createElement('li');
+        const pic = document.createElement("img");
+        const text = document.createTextNode(`${winner}is the winner!`);
+        // pic.setAttribute("src", `${winner}`); commented out- breaking code
+        li.appendChild(text);
+        pokemonWinner.append(li);
+}
+// Reset button- but only clears board. Selects the same pokemon? Need to fix
+const resetPokemon = () => {
+    document.getElementById("two").innerHTML = "";
+    document.getElementById("one").innerHTML = "";
+    document.getElementById("winner").innerHTML = "";
+  }
---
+++ test.js
@@ -0,0 +1,119 @@
+// window.onload = function() {
+// pokemon = [];
+
+// //Pokemon stat = pokemon[0].stats[1].base_stat    <-----attack
+
+// const getPokemonLeft = () => {
+//     fetch('https://pokeapi.co/api/v2/pokemon')
+//     .then(res => res.json())
+//     .then(data => {
+//         let random = Math.floor(Math.random()*20);
+//         return fetch(data.results[random].url)
+//     })
+//     .then(res => res.json())
+//     .then(data => {
+//         pokemon.push(data)
+//         console.log(pokemon);
+//     })
+//     .then(data => {
+//         const pokemonData = data;
+//         const left = document.getElementById("left");
+//         const li = document.createElement("li");
+//         const pic = document.createElement("img");
+//         pic.setAttribute("src", `${pokemon[0].sprites.front_default}`);
+//         li.appendChild(document.createTextNode(pokemon[0].species.name));
+//         li.appendChild(pic);
+//         left.append(li)
+//     })
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pokemon = [];
+// //Pokemon stat = pokemon[0].stats[1].base_stat    <-----attack
+
+// // const getPokemonLeft = () => {
+// //     fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20')
+// //     .then(res => res.json())
+// //     .then(data => {
+// //         let random = Math.floor(Math.random()*20);
+// //         return fetch(data.results[random].url)
+// //     })
+// //     .then(res => res.json())
+// //     .then(data => {
+// //         pokemon.push(data)
+// //         console.log(pokemon);
+// //     })
+// function teamRocket(){
+//     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+//      .then(response => response.json())
+//      .then(function(allpokemon){
+//      allpokemon.results.forEach(function(pokemon){
+//        fetchPokemonData(pokemon); 
+//      })
+//     })
+//    }
+//    function fetchPokemonData(pokemon){
+//     let url = pokemon.url // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+//       fetch(url)
+//       .then(response => response.json())
+//       .then(function(pokeData){
+//       console.log(pokeData)
+//       })
+//     }
+//     function renderPokemon(pokeData){
+//         let allPokemonContainer = document.getElementById('list');
+//         let pokeContainer = document.createElement("div") //div will be used to hold the data/details for indiviual pokemon.{}
+//         let pokeName = document.createElement('h4')
+//         pokeName.innerText = pokeData.name
+//         let pokeNumber = document.createElement('p')
+//         pokeNumber.innerText = `#${pokeData.id}`
+//         let pokeTypes = document.createElement('ul') 
+//         //ul list will hold the pokemon types
+//         createTypes(pokeData.types, pokeTypes) 
+//         // helper function to go through the types array and create li tags for each one
+//         pokeContainer.append(pokeName, pokeNumber, pokeTypes);   
+//         //appending all details to the pokeContainer div
+//         allPokemonContainer.appendChild(pokeContainer);       
+//         //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
+//         }
+
+
+    // .then(data => {
+    //     const pokemonData = data;
+    //     const left = document.getElementById("list");
+    //     const li = document.createElement("li");
+    //     const pic = document.createElement("img");
+    //     pic.setAttribute("src", `${pokemon[0].sprites.front_default}`);
+    //     li.appendChild(document.createTextNode(pokemon[0].species.name));
+    //     li.appendChild(pic);
+    //     left.append(li)
+    // })
